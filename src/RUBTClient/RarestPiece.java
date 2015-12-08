1b1cc7bcec185dc1442c3d2cf9c6a7b70327da45
Ramzy Saleh <rs1064@scarletmail.rutgers.edu>
2015-12-07 19:48:48 -0500

    More work on RarestPiece

--- src/RUBTClient/RarestPiece.java
+++ src/RUBTClient/RarestPiece.java
@@ -8,10 +8,12 @@ public class RarestPiece {
 	private class Piece {
 		int pieceIndex;
 		int rarityValue;
+		boolean done;
 		
 		private Piece(int pieceIndex, int rarityValue){
 			this.pieceIndex = pieceIndex;
 			this.rarityValue = rarityValue;
+			done = false;
 		}
 	}
 
@@ -25,13 +27,81 @@ public class RarestPiece {
 		ordered = new ArrayList<Piece>();
 		this.torrentInfo = tracker.torrentInfo;
 		numPieces = (int)Math.ceil((double)torrentInfo.file_length / (double)torrentInfo.piece_length);
+		initializeRarity();
 	}
 	
 	private void initializeRarity(){
-		
+		Piece p;
 		for(int i = 0; i<numPieces; i++){
-			
+			p = new Piece(i, 0);
+			ordered.add(p);
 		}
 	}
+	
+	private void indexIncrement(int index){
+		
+		int j = 0;
+		if (index>numPieces){ System.out.println("Tried to update piece index that is greater than number of pieces"); }
+		
+		while(j < ordered.size()){
+			if (ordered.get(j).pieceIndex == index){
+				ordered.get(j).rarityValue ++;
+				break;
+			}
+			j++;
+		}
+		sort();
+		
+	}
+	
+	private void indexSet(int index, int rarityVal){
+		
+		int j = 0;
+		if (index>numPieces){ System.out.println("Tried to update piece index that is greater than number of pieces"); }
+		
+		while(j < ordered.size()){
+			if (ordered.get(j).pieceIndex == index){
+				ordered.get(j).rarityValue = rarityVal;
+				break;
+			}
+			j++;
+		}
+		sort();
+		
+	}
+	
+	private void sort(){
+		
+		int count = 0;
+		int currentRarityVal = 0;
+		ArrayList<Piece> temp = new ArrayList<Piece>();
+		
+		while(count < ordered.size()){	
+			for(int j = 0; j < ordered.size(); j++){	
+				if(ordered.get(j).rarityValue == currentRarityVal){
+					temp.add(ordered.get(j));
+					count++;
+				}	
+			}
+			currentRarityVal ++;
+		}
+		
+	}
+	
+	private int findNextPiece(){
+		
+		int j = 0;
+		
+		while(j < ordered.size()){
+			if (ordered.get(j).done == false){
+				ordered.get(j).done = true;
+				return ordered.get(j).pieceIndex;
+			}
+		}
+		
+		return -1;
+		
+	}
+	
 	
 }
